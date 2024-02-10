2010a05da2d8fe6d7fdc277e151b9e5399b02de2
ybgr111 <ybgr111@mail.ru>
2024-02-10 17:00:42 +0300

    Первая неделя

---
+++ grpc/cmd/grpc_client/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	desc "github.com/ybgr111/auth/grpc/pkg/note_v1"
+)
+
+const (
+	address   = "localhost:50052"
+	userID    = 12
+	userName  = "Nikita Ivanov"
+	userEmail = "chertiche@sobaka.budka"
+	userPass  = "blabla"
+	userRole  = 1
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewNoteV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	create, err := c.Create(ctx, &desc.CreateRequest{
+		Info: &desc.UserInfo{
+			Name:  userName,
+			Email: userEmail,
+			Role:  userRole,
+		},
+		Passwd: &desc.UserPassword{
+			Password:        userPass,
+			PasswordConfirm: userPass,
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to create user: %v", err)
+	}
+	log.Printf(color.RedString("Сreate user info:\n"), color.GreenString("%+v", create.GetId()))
+
+	get, err := c.Get(ctx, &desc.GetRequest{Id: userID})
+	if err != nil {
+		log.Fatalf("failed to get user by id: %v", err)
+	}
+	log.Printf(color.RedString("Get user info:\n"), color.GreenString("%+v", get.GetUser()))
+
+	update, err := c.Update(ctx, &desc.UpdateRequest{
+		Id: userID,
+		Info: &desc.UpdateUserInfo{
+			Name:  wrapperspb.String(userName),
+			Email: wrapperspb.String(userEmail),
+			Role:  userRole,
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to update user by id: %v", err)
+	}
+	log.Printf(color.RedString("Update user info:\n"), color.GreenString("%+v", update.String()))
+
+	delete, err := c.Delete(ctx, &desc.DeleteRequest{
+		Id: userID,
+	})
+	if err != nil {
+		log.Fatalf("failed to delete user by id: %v", err)
+	}
+	log.Printf(color.RedString("Delete user info:\n"), color.GreenString("%+v", delete.String()))
+}
